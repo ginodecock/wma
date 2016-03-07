add3edba8c853bf6f01cb490229a06ff76466960
Gino De Cock <ginodecock@skynet.be>
2016-03-07 20:30:44 +0100

    enkel mongo

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,9 @@
 // load all the things we need
 var LocalStrategy    = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
-var TwitterStrategy  = require('passport-twitter').Strategy;
-var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy;
-
 // load up the user model
 var User       = require('../app/models/user');
 
 // load the auth variables
-var configAuth = require('./auth'); // use this one for testing
-
 module.exports = function(passport) {
 
     // =========================================================================
