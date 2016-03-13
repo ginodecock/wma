c20177c47fbb137526136f1988c187a2947a67d0
Gino De Cock <ginodecock@skynet.be>
2016-03-13 10:24:53 +0100

    sensor

--- wma.js
+++ wma.js
@@ -72,7 +72,7 @@ app.post('/wma',function(req,res) {
 			var value3 = parsedBody.value3 || 0;
 			var status = parsedBody.status || 'heartbeat';
 			var currentDate = new Date();
-			currentDate.setTimezone('Europe/Brussels');
+			currentDate.setTimezone('Europe/Amsterdam');
 			var logNow = new Sensorlog({
 				sensorId: chipId,
 				value1: value1,
