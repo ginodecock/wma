831ae0550fea8fb39c33b6bd32480bcaf4913542
Gino De Cock <ginodecock@skynet.be>
2016-03-13 22:08:03 +0100

    sensor

--- app/routes.js
+++ app/routes.js
@@ -83,7 +83,7 @@ module.exports = function(app, passport) {
         console.log(req.body._id);
 
         var query = {"_id": req.body._id};
-        var update = {pbid: req.body.pbid,pbnotify: req.body.pbnotify};
+        var update = {pbid: req.body.pbid,pbnotify: req.body.pbnotify,name: req.body.name,type: req.body.type};
         var options = {new: true};
         Sensor.findOneAndUpdate(query, update, options, function(err, res) {
             if (err) {
