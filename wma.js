d2f2429817149b7d93d2e182a1b94efd4e899b11
Gino De Cock <ginodecock@skynet.be>
2016-03-13 10:32:33 +0100

    chipid

--- wma.js
+++ wma.js
@@ -71,8 +71,7 @@ app.post('/wma',function(req,res) {
 			var value2 = parsedBody.value2 || 0;
 			var value3 = parsedBody.value3 || 0;
 			var status = parsedBody.status || 'heartbeat';
-			var currentDate = new Date();
-			currentDate.setTimezone('Europe/Amsterdam');
+			var currentDate = new DateTime('now',new DateTimeZone("Europe/Brussels"));
 			var logNow = new Sensorlog({
 				sensorId: chipId,
 				value1: value1,
