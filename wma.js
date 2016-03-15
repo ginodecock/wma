a308c203ea09aa5947d001e6ca60f161b85f32d5
Gino De Cock <ginodecock@skynet.be>
2016-03-15 16:23:53 +0100

    sensor

--- wma.js
+++ wma.js
@@ -95,7 +95,7 @@ app.post('/wma',function(req,res) {
             	if (sensorres.pbnotify && sensorres.pbid != ""){
             		console.log(sensorres.pbid);
             		var pusher = new PushBullet(sensorres.pbid);
-            		pusher.note('', 'noteTitle', 'noteBody', function(error, response) {
+            		pusher.note('', parsedBody.name, parsedBody.status + '! Water usage = ' + parsedBody.v1 + '@ Temperature = ' + parsedBody.v2 , function(error, response) {
 						console.log(response); 
 					});
             	}
