642ed7bc5236364c88cb3682e17fe7877e4c7458
Gino De Cock <ginodecock@skynet.be>
2016-03-28 18:04:13 +0200

    sensor

--- app/routes.js
+++ app/routes.js
@@ -62,7 +62,7 @@ module.exports = function(app, passport) {
 
 
         if (req.body.request == "Log"){
-            Sensorlog.find({ $query: {sensorId:req.body.sensorId, status:"log"}, $orderby:{timestamp:-1}},{},{limit:10} function(err, sensorlogs){
+            Sensorlog.find({ $query: {sensorId:req.body.sensorId, status:"log"}, $orderby:{timestamp:-1}},{},{limit:10}, function(err, sensorlogs){
                 if (err) throw err;
                 console.log(sensor);
 
