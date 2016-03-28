b396edf0ad2b1fc172fffbc8e3d3023a5514271a
Gino De Cock <ginodecock@skynet.be>
2016-03-28 17:13:15 +0200

    sensor

--- app/routes.js
+++ app/routes.js
@@ -62,7 +62,7 @@ module.exports = function(app, passport) {
 
 
         if (req.body.request == "Log"){
-            Sensorlog.find({ $query: {sensorId:req.body.sensorId, status:"log"}, $orderby:{timestamp:-1}}, { skip: 0, limit: 100 },function(err, sensorlogs){
+            Sensorlog.find({ $query: {sensorId:req.body.sensorId, status:"log"}, $orderby:{timestamp:-1}},function(err, sensorlogs){
                 if (err) throw err;
                 console.log(sensor);
 
@@ -70,7 +70,7 @@ module.exports = function(app, passport) {
                     sensor: sensor,
                     sensorlogs: sensorlogs,
                 });
-            });
+            }).limit(100);
         }
         if (req.body.request == "Alarm"){
             Sensorlog.find({ $query: {sensorId:req.body.sensorId, status: /^alarm/}, $orderby:{timestamp:-1}}, function(err, sensorlogs){
