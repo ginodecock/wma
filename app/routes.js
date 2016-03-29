4bc80d16c89cb262e3925bf991a2503ab4dfc8d0
Gino De Cock <ginodecock@skynet.be>
2016-03-29 20:18:30 +0200

    sensor

--- app/routes.js
+++ app/routes.js
@@ -83,13 +83,13 @@ module.exports = function(app, passport) {
             });
         }
         if (req.body.request == "Graph"){
-            Sensorlog.find({ $query: {sensorId:req.body.sensorId, status:"log"}, $natural:1},{},{limit:100}, function(err, sensorlogs){
+            Sensorlog.find({ $query: {sensorId:req.body.sensorId, status:"log"}, $natural:-1},{},{limit:100}, function(err, sensorlogs){
                 if (err) throw err;
                 console.log(sensor);
 
                 res.render('wmasensorgraph.ejs',{
                     sensor: sensor,
-                    sensorlogs: sensorlogs
+                    sensorlogs: sensorlogs.reverse()
                 });
             });
         }
